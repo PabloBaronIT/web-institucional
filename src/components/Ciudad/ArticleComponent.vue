8b61795ec39b7e2dd56557e75c117477bd3a468c
OficinaVirtualBaron <patriciotrabajo@gmail.com>
2022-12-06 18:47:35 -0300

    Check opacity

--- src/components/Ciudad/ArticleComponent.vue
+++ src/components/Ciudad/ArticleComponent.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="article-container">
-    <transition>
+    <transition name="fade">
+      <div class="">
+        <h4 style="color: var(--red)">EL CORAZÓN DE ARGENTINA</h4>
+        <h2>Tierra de origen jesuita y de raíces profundas</h2>
+      </div>
+    </transition>
+    <transition name="fade">
+      <div class="">
+        <h4 style="color: var(--red)">EL CORAZÓN DE ARGENTINA</h4>
+        <h2>Tierra de origen jesuita y de raíces profundas</h2>
+      </div>
+    </transition>
+    <transition name="fade">
+      <div class="">
+        <h4 style="color: var(--red)">EL CORAZÓN DE ARGENTINA</h4>
+        <h2>Tierra de origen jesuita y de raíces profundas</h2>
+      </div>
+    </transition>
+    <transition name="fade">
       <div class="">
         <h4 style="color: var(--red)">EL CORAZÓN DE ARGENTINA</h4>
         <h2>Tierra de origen jesuita y de raíces profundas</h2>
@@ -18,5 +36,15 @@ export default {
 <style scoped>
 .article-container {
   padding: 20px;
+}
+
+.fade-leave-top {
+  opacity: 0;
+}
+
+.fade-enter-to,
+.fade-enter-active,
+.fade-enter-from {
+  transition: opacity 1s ease;
 }
 </style>
